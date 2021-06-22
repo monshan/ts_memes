e6f771ec19744f388c828ec2ace739060a600d3f
Marika <marikashanahan@gmail.com>
2021-06-22 16:48:40 -0600

    Write boilerplate header and main for App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import { useState , useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+
   return (
     <div className="App">
-     
+      <header>
+        <h1>The Kittie Grid Generator</h1>
+        <p>Using TypeScript</p>
+      </header>
+      <main>
+        <form>
+          <input type="number" id="inputWidth" required />
+          <label htmlFor="inputWidth">px width</label>
+          <input type="number" id="inputHeight" required />
+          <label htmlFor="inputHeight">px height</label>
+          <button>Generate</button>
+        </form>
+      </main>
     </div>
   );
 }
